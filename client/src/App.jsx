6eefaa0acc634f3d7d9c6bd75fa6b72be0cddb27
Ashishdevelopr <ashishdevelopr@gmail.com>
2023-08-03 21:40:15 +0530

    Adding routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,18 @@ import './App.css'
 import Form from './components/Form'
 import Navbar from './components/Navbar'
 import Table from './components/Table'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <div className="layout">
-        {/* <Form /> */}
-        <Table/>
+        <Routes>
+          <Route path="/art-gallery" element={<Table />} />
+          <Route path="/add-art" element={<Form />} />
+        </Routes>
       </div>
       <footer className='footer'><a href='https://github.com/Ashishdevelopr' className='footer-link' target='_blank'>Crafted with passion by  <span className='footer-span'><i className="fa-brands fa-github"></i></span> Ashish Developer</a></footer>
     </>
